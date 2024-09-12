45671be2bfbcedd3ac8c9867ba91a4177b7f9198
Elza Sharapova <elzananakidonskaya@yandex.ru>
2024-09-12 20:58:36 +0300

    fix: add boxshadow

--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -4,14 +4,14 @@ import {Link as RouterLink} from "react-router-dom";
 function Navigation() {
     return (
         <Box component="section" sx={{display: 'flex', gap: '15px'}}>
-            <Card>
+            <Card sx={{boxShadow: '-1px 0px 10px 4px rgba(0,0,0,0.74)'}}>
                 <CardContent>
                     <Button component={RouterLink} to="/advertisements" color="primary">
                         Мои объявления
                     </Button>
                 </CardContent>
             </Card>
-            <Card>
+            <Card sx={{boxShadow: '-1px 0px 10px 4px rgba(0,0,0,0.74)'}}>
                 <CardContent>
                     <Button component={RouterLink} to="/orders">
                         Мои заказы
